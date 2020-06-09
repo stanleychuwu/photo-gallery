c8f51217e385476108f0ab3bb27816fd24357dca
Stanley Wu <stanleychuwu@gmail.com>
2020-06-08 18:18:43 -0700

    Added pics in file and clienT

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   render () {
     return (
-      <div>Hi from App</div>
+      <div id='pics'>
+        <img src='../pics/pic1.jpg'></img>
+        <img src='../pics/pic2.jpg'></img>
+        <img src='../pics/pic3.jpg'></img>
+        <img src='../pics/pic4.jpg'></img>
+        <img src='../pics/pic5.jpg'></img>
+      </div>
     )
   }
 }
